4b5867406aeb92e7bac8499cebe9f7d9cd147033
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-01-03 14:57:40 +0400

    --

--- routes/v1.js
+++ routes/v1.js
@@ -5,18 +5,19 @@ var unicoin = require('../gateway/unicoin');
 
 router.get('/pubticker/:symbol', function (req, res, next) {
 
-  bitfinex.pubtickerPromise(req.params.symbol).then( (res, rej) => {
-
-    
-
-  });
-
-  bitfinex.pubticker(req.params.symbol, function (err, response) {
-    if (err)
-      res.json({ error: err });
-    else
+  bitfinex.pubtickerPromise(req.params.symbol)
+    .then((response) => {
       res.json(response);
-  });
+    }).catch(err => {
+      res.json({ error: err });
+    });
+
+  // bitfinex.pubticker(req.params.symbol, function (err, response) {
+  //   if (err)
+  //     res.json({ error: err });
+  //   else
+  //     res.json(response);
+  // });
 });
 
 router.get('/account_infos', function (req, res, next) {
